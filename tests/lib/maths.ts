97d02b466696a5495a95c3e3a8d00fc4ff0c2f48
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-12-18 18:33:40 +0000

    Fix test functions

--- tests/lib/maths.ts
+++ tests/lib/maths.ts
@@ -4,8 +4,8 @@ import * as maths from "@src/lib/maths" // Absolute import. The same everywhere
 test("Maths", (t) => {
 	t.plan(4);
 
-	t.ok(maths.add(2, 2), 4, "adds correctly");
-	t.ok(maths.subtract(2, 2), 0, "subtracts correctly");
-	t.ok(maths.multiply(2, 3), 6, "multiplies correctly");
-	t.ok(maths.divide(2, 2), 1, "divides correctly");
+	t.equal(maths.add(2, 2), 4, "adds correctly");
+	t.equal(maths.subtract(2, 2), 0, "subtracts correctly");
+	t.equal(maths.multiply(2, 3), 6, "multiplies correctly");
+	t.equal(maths.divide(2, 2), 1, "divides correctly");
 })
