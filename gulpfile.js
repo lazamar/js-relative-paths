647cb86dde0819e821131c6122b4c0eda5b55118
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-12-20 00:52:08 +0000

    Cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,44 +4,43 @@ const browserify = require("browserify");
 const tsify = require("tsify");
 const sass = require("gulp-sass");
 
-const shimify = function(b, shim) {
-    b._bresolve = (function(resolve) {
-        return function(id, parent, cb) {
-            // not undefined and false
-            if (parent.inNodeModules === false) {
-                return resolve(shim(id, parent), parent, cb);
-            }
-            return resolve(id, parent, cb);
-        };
-    })(b._bresolve);
+// -------------------- TYPESCRIPT -----------------------------
+
+const preprocessPaths = (b, preprocessor) => {
+    const resolve = b._bresolve;
+
+    b._bresolve = (id, parent, cb) =>
+        parent.inNodeModules === false
+            ? resolve(preprocessor(id, parent), parent, cb)
+            : resolve(id, parent, cb);
 };
 
-const shimF = (importPath, importerModule) => {
+const pathPreprocessor = (importPath, importerModule) => {
     try {
         return ws.path(importPath);
     } catch (e) {
         throw new Error(
             `\n\n\tCannot import "${importPath}" from "${importerModule.id}.\n\t${e.message}\n`
         );
-        // DO Nothing
     }
 };
 
 gulp.task("ts", () => {
-	const entry = ws.path("@browser/index.ts");
-	const outname = "typescript.js";
+    const entry = ws.path("@browser/index.ts");
+    const outname = "typescript.js";
 
-	return browserify({ entries: [entry], outputName: outname })
-	   .plugin(shimify, shimF)
-	   .plugin(tsify)
-       .bundle()
-       // We are just outputting stuff to stdout to make things simpler
-       .pipe(process.stdout);
+    browserify({ entries: [entry], outputName: outname })
+        .plugin(preprocessPaths, pathPreprocessor)
+        .plugin(tsify)
+        .bundle()
+        // We are just outputting stuff to stdout to make things simpler
+        .pipe(process.stdout);
 });
 
+// ----------------------- SASS -----------------------------
 
 // is a node modules if import path starts with a letter
-const isNodeModule = p => /^[a-zA-Z]/.test(p);
+const isNodeModule = p => /^[a-z@]/.test(p);
 
 const workspaceImporter = (importPath, importer) => {
     if (isNodeModule(importPath)) {
@@ -60,17 +59,18 @@ const workspaceImporter = (importPath, importer) => {
 };
 
 gulp.task("sass", () => {
-	const entry = ws.path("@styles/index.scss");
-	const destPath = ws.path("@build");
+    const entry = ws.path("@styles/index.scss");
+    const destPath = ws.path("@build");
 
-  return gulp
-      .src(entry)
-      .pipe(sass({
-              // Allows us to @import things in `node_modules` directly
-              includePaths: ["node_modules"],
-              // This importer allows us to use workspace paths
-              importer: workspaceImporter
-          })
-      )
-      .pipe(gulp.dest(destPath));
+    return gulp
+        .src(entry)
+        .pipe(
+            sass({
+                // Allows us to @import things in `node_modules` directly
+                includePaths: ["node_modules"],
+                // This importer allows us to use workspace paths
+                importer: workspaceImporter
+            })
+        )
+        .pipe(gulp.dest(destPath));
 });
