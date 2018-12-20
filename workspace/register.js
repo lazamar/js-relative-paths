7119e30828ad6ce80ae0e54167f264a00cf72162
Marcelo Lazaroni <lazaronijunior@gmail.com>
2018-12-20 00:59:25 +0000

    Remove eslint

--- workspace/register.js
+++ workspace/register.js
@@ -1,2 +1,10 @@
+// Enhance node's require so that it can handle typescript paths.
+//
+// This file is to be required at the beginning 
+// of an node app, or even before that by using the `-r`
+// flag in node like this:
+//
+//     node -r ./workspace/register myModules/index.ts
+//
 const workspace = require("./index");
 workspace.register();
